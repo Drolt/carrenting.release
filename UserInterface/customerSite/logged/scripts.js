26e607d2d70d27173b117f55399e7f4f2407e37d
Drolt <thomas_klug@gmx.de>
2024-01-13 11:15:56 +0100

    random prices added

--- UserInterface/customerSite/logged/scripts.js
+++ UserInterface/customerSite/logged/scripts.js
@@ -174,10 +174,15 @@ function populateAvailableCarsTable(availableCars) {
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
 
     cell1.innerHTML = car.carID;
     cell2.innerHTML = car.brand;
     cell3.innerHTML = car.model;
+
+    // Generate a random price per day
+    var randomPricePerDay = Math.floor(Math.random() * 151) + 50; // Random number between 50 and 200
+    cell4.innerHTML = randomPricePerDay + " €/day";
   });
 }
 
